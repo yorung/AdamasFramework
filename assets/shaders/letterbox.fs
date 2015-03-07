32ebfb32b15f1ac289b9b6beb9fb6561e9c7db61
yorung <yorung@gmail.com>
2015-03-07 18:27:48 +0900

    float literals with f is illegal for ES 2.0

--- assets/shaders/letterbox.fs
+++ assets/shaders/letterbox.fs
@@ -4,6 +4,6 @@ varying vec2 texcoord;
 uniform sampler2D sampler;
 void main() {
 	vec2 coord = vec2(texcoord.x, texcoord.y);
-	float brightness = position.y < -0.8f || position.y > 0.8f ? 0.1f : 1.0f;
+	float brightness = position.y < -0.8 || position.y > 0.8 ? 0.1 : 1.0;
 	gl_FragColor = vec4(texture2D(sampler, coord).xyz * brightness, 1.0);
 }
