bbafaa7147445bbb77007eba27fc257d5afb2868
yorung <yorung@gmail.com>
2015-03-22 19:45:04 +0900

    remove DirectX related code

--- cpp/mesh_renderer.cpp
+++ cpp/mesh_renderer.cpp
@@ -103,96 +103,6 @@ void MeshRenderer::Init(const Block& block)
 	glBufferData(GL_DRAW_INDIRECT_BUFFER, cmds.size() * sizeof(DrawElementsIndirectCommand), &cmds[0], GL_STATIC_DRAW);
 	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
 }
-/*
-void MeshRenderer::Calc(const Mat BoneMatrices[BONE_MAX], const Block& block) const
-{
-	ID3D11ShaderResourceView* srvPos;
-	ID3D11ShaderResourceView* srvSkin;
-	{
-		D3D11_SHADER_RESOURCE_VIEW_DESC desc;
-		memset(&desc, 0, sizeof(desc));
-		desc.ViewDimension = D3D11_SRV_DIMENSION_BUFFEREX;
-		desc.Format = DXGI_FORMAT_UNKNOWN;
-		desc.BufferEx.FirstElement = 0;
-		desc.BufferEx.NumElements = block.vertices.size();
-		HRESULT hr = deviceMan11.GetDevice()->CreateShaderResourceView(posBuffer, &desc, &srvPos);
-		hr = deviceMan11.GetDevice()->CreateShaderResourceView(skinBuffer, &desc, &srvSkin);
-	}
-
-	ID3D11UnorderedAccessView* uav;
-	{
-		D3D11_UNORDERED_ACCESS_VIEW_DESC desc;
-		memset(&desc, 0, sizeof(desc));
-		desc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
-		desc.Format = DXGI_FORMAT_UNKNOWN;
-		desc.Buffer.FirstElement = 0;
-		desc.Buffer.NumElements = block.vertices.size();
-		HRESULT hr = deviceMan11.GetDevice()->CreateUnorderedAccessView(skinnedPosBuffer, &desc, &uav);
-	}
-
-	computeShaderSkinning.Dispatch(BoneMatrices, srvPos, srvSkin, uav, block.vertices.size());
-	SAFE_RELEASE(srvPos);
-	SAFE_RELEASE(srvSkin);
-	SAFE_RELEASE(uav);
-}
-
-void MeshRenderer11::Draw(const Mat BoneMatrices[BONE_MAX], int nBones, const Block& block) const
-{
-	Calc(BoneMatrices, block);
-
-	deviceMan11.GetContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	shaderMan.Apply(shaderId);
-
-	Mat matWorld, matView, matProj;
-	matrixMan.Get(MatrixMan::WORLD, matWorld);
-	matrixMan.Get(MatrixMan::VIEW, matView);
-	matrixMan.Get(MatrixMan::PROJ, matProj);
-
-	deviceMan11.GetContext()->OMSetDepthStencilState(pDSState, 1);
-	deviceMan11.GetContext()->PSSetSamplers(0, 1, &pSamplerState);
-
-	deviceMan11.GetContext()->IASetIndexBuffer(pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
-	UINT strides[] = { sizeof(MeshColor) };
-	UINT offsets[] = { 0 };
-	deviceMan11.GetContext()->IASetVertexBuffers(0, 1, &colorBuffer, strides, offsets);
-
-	ID3D11ShaderResourceView* srvSkinnedPos;
-	{
-		D3D11_SHADER_RESOURCE_VIEW_DESC desc;
-		memset(&desc, 0, sizeof(desc));
-		desc.ViewDimension = D3D11_SRV_DIMENSION_BUFFEREX;
-		desc.Format = DXGI_FORMAT_UNKNOWN;
-		desc.BufferEx.FirstElement = 0;
-		desc.BufferEx.NumElements = block.vertices.size();
-		HRESULT hr = deviceMan11.GetDevice()->CreateShaderResourceView(skinnedPosBuffer, &desc, &srvSkinnedPos);
-	}
-	deviceMan11.GetContext()->VSSetShaderResources(0, 1, &srvSkinnedPos);
-	SAFE_RELEASE(srvSkinnedPos);
-
-	for (int j = 0; (unsigned)j < block.materialMaps.size(); j++) {
-		const MaterialMap& matMap = block.materialMaps[j];
-		const Material* mat = matMan.Get(matMap.materialId);
-		assert(mat);
-		ID3D11ShaderResourceView* tx = texMan.Get(mat->tmid);
-		deviceMan11.GetContext()->PSSetShaderResources(0, 1, &tx);
-
-		MeshConstantBuffer cBuf;
-		cBuf.matW = matWorld;
-		cBuf.matV = matView;
-		cBuf.matP = matProj;
-		cBuf.faceColor = mat->faceColor;
-		cBuf.emissive = mat->emissive;
-		cBuf.camPos = fastInv(matView).GetRow(3);
-		bufferMan.Write(constantBufferId, &cBuf);
-		const auto buf = bufferMan.Get(constantBufferId);
-		deviceMan11.GetContext()->VSSetConstantBuffers(0, 1, &buf);
-		deviceMan11.GetContext()->PSSetConstantBuffers(0, 1, &buf);
-
-		deviceMan11.GetContext()->DrawIndexed(matMap.faces * 3, matMap.faceStartIndex * 3, 0);
-	}
-}
-
-*/
 
 void MeshRenderer::Draw(const Mat BoneMatrices[BONE_MAX], int nBones, const Block& block) const
 {
@@ -231,19 +141,6 @@ void MeshRenderer::Draw(const Mat BoneMatrices[BONE_MAX], int nBones, const Bloc
 		assert(mat);
 
 		glBindTexture(GL_TEXTURE_2D, mat->tmid);
-		/*
-		MeshConstantBuffer cBuf;
-		cBuf.matW = matWorld;
-		cBuf.matV = matView;
-		cBuf.matP = matProj;
-		cBuf.faceColor = mat->faceColor;
-		cBuf.emissive = mat->emissive;
-		cBuf.camPos = fastInv(matView).GetRow(3);
-		bufferMan.Write(constantBufferId, &cBuf);
-		const auto buf = bufferMan.Get(constantBufferId);
-		deviceMan11.GetContext()->VSSetConstantBuffers(0, 1, &buf);
-		deviceMan11.GetContext()->PSSetConstantBuffers(0, 1, &buf);
-		*/
 
 	//	afDrawIndexedTriangleList(pIndexBuffer, matMap.faces * 3, matMap.faceStartIndex * 3);
 
