1d1c952b9f89ba8f330576fdd2c801f15707570b
yorung <yorung@gmail.com>
2015-05-23 22:07:45 +0900

    modify commandline batches

---
+++ AndroidStudio/bir.bat
@@ -0,0 +1,3 @@
+call gradlew.bat build
+call install.bat
+call run.bat
--- AndroidStudio/run.bat
+++ AndroidStudio/run.bat
@@ -2,8 +2,7 @@ adb logcat -c
 adb shell am start -a android.intent.action.MAIN -n com.pinotnoir.adamas/common.pinotnoir.glactivity.PinotGLActivity
 
 for /f "skip=1 delims=" %%x in ('wmic os get localdatetime') do if not defined X set X=%%x
-echo %X:~0,14%
 
-mkdir $logs
+if not exist $logs mkdir $logs
 adb logcat > $logs\$logcat_%X:~0,14%.log
 pause
