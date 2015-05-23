9ecb7fe9a96af5241af530486c18af21b4034be4
yorung <yorung@gmail.com>
2015-05-23 17:10:44 +0900

    write logcat as a file with time stamp

--- AndroidStudio/run.bat
+++ AndroidStudio/run.bat
@@ -1,1 +1,9 @@
+adb logcat -c
 adb shell am start -a android.intent.action.MAIN -n com.pinotnoir.adamas/common.pinotnoir.glactivity.PinotGLActivity
+
+for /f "skip=1 delims=" %%x in ('wmic os get localdatetime') do if not defined X set X=%%x
+echo %X:~0,14%
+
+mkdir $logs
+adb logcat > $logs\$logcat_%X:~0,14%.log
+pause
