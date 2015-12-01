f7ce0c1855d80991f0b5a853fed9d58e40df879e
yorung <yorung@gmail.com>
2015-12-02 00:13:32 +0900

    Linked OpenSL ES library

--- cpp/voice_android.cpp
+++ cpp/voice_android.cpp
@@ -1,5 +1,34 @@
 #include "stdafx.h"
 
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+
+template <class T> void SAFE_DESTROY(T& p) {
+	if (p) {
+		(*p)->Destroy(p);
+		p = nullptr;
+	}
+}
+
+class SL {
+	SLObjectItf engineObject = nullptr;
+public:
+	SL() {
+		SLresult r = slCreateEngine(&engineObject, 0, nullptr, 0, nullptr, nullptr);
+		assert(r == SL_RESULT_SUCCESS);
+		aflog("slCreateEngine");
+		r = (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
+		assert(r == SL_RESULT_SUCCESS);
+		aflog("engineObject->Realize");
+	}
+	~SL() {
+		SAFE_DESTROY(engineObject);
+		aflog("engineObject destroyed");
+	}
+};
+
+static SL sl;
+
 void Voice::Create(const char*)
 {
 }
