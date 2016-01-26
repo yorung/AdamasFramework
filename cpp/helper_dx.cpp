544ea046b918372c0d76e4818b61d815f56e2a00
yorung <yorung@gmail.com>
2016-01-27 01:39:00 +0900

    Merge latest helper_dx from dx11bvh project

--- cpp/helper_dx.cpp
+++ cpp/helper_dx.cpp
@@ -1,72 +1,71 @@
 #include "stdafx.h"
 
 #ifdef __d3d11_h__
-ID3D11Buffer* afCreateIndexBuffer(const AFIndex* indi, int numIndi)
+IBOID afCreateIndexBuffer(const AFIndex* indi, int numIndi)
 {
-	ID3D11Buffer* indexBuffer;
+	IBOID indexBuffer;
 	D3D11_SUBRESOURCE_DATA subresData = { indi, 0, 0 };
 	deviceMan11.GetDevice()->CreateBuffer(&CD3D11_BUFFER_DESC(numIndi * sizeof(AFIndex), D3D11_BIND_INDEX_BUFFER), &subresData, &indexBuffer);
 	return indexBuffer;
 }
 
-ID3D11Buffer* afCreateVertexBuffer(int size, const void* data)
+VBOID afCreateVertexBuffer(int size, const void* data)
 {
-	ID3D11Buffer* vbo;
+	VBOID vbo;
 	D3D11_SUBRESOURCE_DATA subresData = { data, 0, 0 };
 	deviceMan11.GetDevice()->CreateBuffer(&CD3D11_BUFFER_DESC(size, D3D11_BIND_VERTEX_BUFFER), &subresData, &vbo);
 	return vbo;
 }
 
-ID3D11Buffer* afCreateDynamicVertexBuffer(int size)
+VBOID afCreateDynamicVertexBuffer(int size)
 {
-	ID3D11Buffer* vbo;
+	VBOID vbo;
 	deviceMan11.GetDevice()->CreateBuffer(&CD3D11_BUFFER_DESC(size, D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE), nullptr, &vbo);
 	return vbo;
 }
 
 UBOID afCreateUBO(int size)
 {
-	ID3D11Buffer* ubo;
+	UBOID ubo;
 	deviceMan11.GetDevice()->CreateBuffer(&CD3D11_BUFFER_DESC(size, D3D11_BIND_CONSTANT_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE), nullptr, &ubo);
 	return ubo;
 }
 
-SAMPLERID afCreateSampler()
+SAMPLERID afCreateSampler(SamplerFilter filter, SamplerWrap wrap)
 {
 	D3D11_SAMPLER_DESC  desc;
 	ZeroMemory(&desc, sizeof(desc));
-	desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-	desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	desc.AddressU = wrap;
+	desc.AddressV = wrap;
 	desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	desc.MaxAnisotropy = 1;
-	desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	desc.Filter = filter;
 	desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	desc.MaxLOD = D3D11_FLOAT32_MAX;
-	ID3D11SamplerState* sampler;
+	ComPtr<ID3D11SamplerState> sampler;
 	deviceMan11.GetDevice()->CreateSamplerState(&desc, &sampler);
 	return sampler;
 }
 
 void afBindBufferToBindingPoint(UBOID ubo, UINT uniformBlockBinding)
 {
-	deviceMan11.GetContext()->VSSetConstantBuffers(uniformBlockBinding, 1, &ubo);
-	deviceMan11.GetContext()->PSSetConstantBuffers(uniformBlockBinding, 1, &ubo);
+	deviceMan11.GetContext()->VSSetConstantBuffers(uniformBlockBinding, 1, ubo.GetAddressOf());
+	deviceMan11.GetContext()->PSSetConstantBuffers(uniformBlockBinding, 1, ubo.GetAddressOf());
 }
 
 void afBindTextureToBindingPoint(TexMan::TMID tex, UINT textureBindingPoint)
 {
-	ID3D11ShaderResourceView* tx = texMan.Get(tex);
-	deviceMan11.GetContext()->VSSetShaderResources(textureBindingPoint, 1, &tx);
-	deviceMan11.GetContext()->PSSetShaderResources(textureBindingPoint, 1, &tx);
+	ComPtr<ID3D11ShaderResourceView> tx = texMan.Get(tex);
+	deviceMan11.GetContext()->PSSetShaderResources(textureBindingPoint, 1, tx.GetAddressOf());
 }
 
-void afBindSamplerToBindingPoint(ID3D11SamplerState* sampler, UINT textureBindingPoint)
+void afBindSamplerToBindingPoint(SAMPLERID sampler, UINT textureBindingPoint)
 {
-	deviceMan11.GetContext()->VSSetSamplers(textureBindingPoint, 1, &sampler);
-	deviceMan11.GetContext()->PSSetSamplers(textureBindingPoint, 1, &sampler);
+	deviceMan11.GetContext()->VSSetSamplers(textureBindingPoint, 1, sampler.GetAddressOf());
+	deviceMan11.GetContext()->PSSetSamplers(textureBindingPoint, 1, sampler.GetAddressOf());
 }
 
-void afWriteBuffer(ID3D11Buffer* p, const void* buf, int size)
+void afWriteBuffer(const IBOID p, const void* buf, int size)
 {
 #ifdef _DEBUG
 	D3D11_BUFFER_DESC desc;
@@ -76,9 +75,9 @@ void afWriteBuffer(ID3D11Buffer* p, const void* buf, int size)
 	}
 #endif
 	D3D11_MAPPED_SUBRESOURCE m;
-	HRESULT hr = deviceMan11.GetContext()->Map(p, 0, D3D11_MAP_WRITE_DISCARD, 0, &m);
+	HRESULT hr = deviceMan11.GetContext()->Map(p.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &m);
 	memcpy(m.pData, buf, size);
-	deviceMan11.GetContext()->Unmap(p, 0);
+	deviceMan11.GetContext()->Unmap(p.Get(), 0);
 }
 
 
@@ -99,8 +98,14 @@ IBOID afCreateQuadListIndexBuffer(int numQuads)
 
 void afDrawIndexedTriangleList(int numIndices, int start)
 {
-	deviceMan11.GetContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	deviceMan11.GetContext()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	deviceMan11.GetContext()->DrawIndexed(numIndices, start, 0);
+}
+
+void afDrawTriangleStrip(int numVertices, int start)
+{
+	deviceMan11.GetContext()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	deviceMan11.GetContext()->Draw(numVertices, start);
 }
 
 void afEnableBackFaceCulling(bool cullBack)
@@ -148,9 +153,52 @@ void afDepthStencilMode(bool depth)
 	SAFE_RELEASE(ds);
 }
 
-VAOID afCreateVAO(ShaderMan::SMID program, const InputElement elements[], int numElements, int numBuffers, VBOID const* vertexBufferIds, const int* strides, IBOID ibo)
+VAOID afCreateVAO(ShaderMan::SMID program, const InputElement elements[], int numElements, int numBuffers, VBOID* const vertexBufferIds, const int* strides, IBOID ibo)
 {
 	return new FakeVAO(program, elements, numElements, numBuffers, vertexBufferIds, strides, nullptr, ibo);
 }
 
+void AFRenderTarget::InitForDefaultRenderTarget()
+{
+	Destroy();
+	renderTargetView = deviceMan11.GetDefaultRenderTarget();
+	renderTargetView->AddRef();
+	depthStencilView = deviceMan11.GetDefaultDepthStencil();
+	depthStencilView->AddRef();
+}
+
+void AFRenderTarget::Init(ivec2 size, DXGI_FORMAT colorFormat)
+{
+	Destroy();
+	texSize = size;
+	CD3D11_TEXTURE2D_DESC tDesc(colorFormat, size.x, size.y, 1, 1, D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS);
+	ID3D11Texture2D* tex;
+	HRESULT hr = deviceMan11.GetDevice()->CreateTexture2D(&tDesc, NULL, &tex);
+	CD3D11_RENDER_TARGET_VIEW_DESC rDesc(D3D11_RTV_DIMENSION_TEXTURE2D, tDesc.Format);
+	hr = deviceMan11.GetDevice()->CreateRenderTargetView(tex, &rDesc, &renderTargetView);
+	CD3D11_SHADER_RESOURCE_VIEW_DESC sDesc(D3D11_SRV_DIMENSION_TEXTURE2D, tDesc.Format);
+	hr = deviceMan11.GetDevice()->CreateShaderResourceView(tex, &sDesc, &shaderResourceView);
+	CD3D11_UNORDERED_ACCESS_VIEW_DESC uDesc(D3D11_UAV_DIMENSION_TEXTURE2D, tDesc.Format);
+	hr = deviceMan11.GetDevice()->CreateUnorderedAccessView(tex, &uDesc, &unorderedAccessView);
+	SAFE_RELEASE(tex);
+
+	depthStencilView = deviceMan11.GetDefaultDepthStencil();
+	depthStencilView->AddRef();
+}
+
+void AFRenderTarget::Destroy()
+{
+	SAFE_RELEASE(renderTargetView);
+	SAFE_RELEASE(shaderResourceView);
+	SAFE_RELEASE(unorderedAccessView);
+	SAFE_RELEASE(depthStencilView);
+}
+
+void AFRenderTarget::BeginRenderToThis()
+{
+	deviceMan11.GetContext()->OMSetRenderTargets(1, &renderTargetView, depthStencilView);
+	float clearColor[4] = { 0.0f, 0.2f, 0.0f, 0.0f };
+	deviceMan11.GetContext()->ClearRenderTargetView(renderTargetView, clearColor);
+	deviceMan11.GetContext()->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+}
 #endif
--- cpp/helper_dx.h
+++ cpp/helper_dx.h
@@ -16,34 +16,43 @@ public:
 	}
 };
 
-
 typedef unsigned short AFIndex;
 
 #define AFIndexTypeToDevice DXGI_FORMAT_R16_UINT
-typedef ID3D11Buffer* IBOID;
-typedef ID3D11Buffer* VBOID;
-typedef ID3D11Buffer* UBOID;
-typedef FakeVAO* VAOID;
-typedef ID3D11SamplerState* SAMPLERID;
+typedef ComPtr<ID3D11Buffer> IBOID;
+typedef ComPtr<ID3D11Buffer> VBOID;
+typedef ComPtr<ID3D11Buffer> UBOID;
+typedef ComPtr<ID3D11SamplerState> SAMPLERID;
 
-#define afSafeDeleteBuffer SAFE_RELEASE
-#define afSafeDeleteSampler SAFE_RELEASE
+inline void afSafeDeleteBuffer(ComPtr<ID3D11Buffer>& p) { p.Reset(); }
+inline void afSafeDeleteSampler(SAMPLERID& p) { p.Reset(); }
 #define afSafeDeleteVAO SAFE_DELETE
-#define afSafeDeleteSampler SAFE_RELEASE
 
-void afWriteBuffer(ID3D11Buffer* p, const void* buf, int size);
+void afWriteBuffer(const IBOID p, const void* buf, int size);
 
 IBOID afCreateIndexBuffer(const AFIndex* indi, int numIndi);
 IBOID afCreateQuadListIndexBuffer(int numQuads);
 VBOID afCreateVertexBuffer(int size, const void* buf);
 VBOID afCreateDynamicVertexBuffer(int size);
 UBOID afCreateUBO(int size);
-SAMPLERID afCreateSampler();
+
+#define SamplerWrap D3D11_TEXTURE_ADDRESS_MODE
+#define SW_REPEAT D3D11_TEXTURE_ADDRESS_WRAP
+#define SW_CLAMP D3D11_TEXTURE_ADDRESS_CLAMP
+
+#define SamplerFilter D3D11_FILTER
+#define SF_POINT D3D11_FILTER_MIN_MAG_MIP_POINT
+#define SF_LINEAR D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT
+#define SF_MIPMAP D3D11_FILTER_MIN_MAG_MIP_LINEAR
+
+SAMPLERID afCreateSampler(SamplerFilter samplerFilter, SamplerWrap wrap);
+
 void afBindBufferToBindingPoint(UBOID ubo, UINT uniformBlockBinding);
 void afBindTextureToBindingPoint(TexMan::TMID tex, UINT textureBindingPoint);
-void afBindSamplerToBindingPoint(ID3D11SamplerState*, UINT textureBindingPoint);
+void afBindSamplerToBindingPoint(SAMPLERID sampler, UINT textureBindingPoint);
 
 void afDrawIndexedTriangleList(int numIndices, int start = 0);
+void afDrawTriangleStrip(int numVertices, int start = 0);
 void afEnableBackFaceCulling(bool cullBack);
 
 enum BlendMode {
@@ -53,5 +62,31 @@ enum BlendMode {
 void afBlendMode(BlendMode mode);
 void afDepthStencilMode(bool depth);
 
-VAOID afCreateVAO(ShaderMan::SMID program, const InputElement elements[], int numElements, int numBuffers, VBOID const *vertexBufferIds, const int* strides, IBOID ibo);
-inline void afBindVAO(VAOID vao) { vao->Apply(); }
+enum AFDTFormat
+{
+	AFDT_INVALID,
+	AFDT_R8G8B8A8_UINT,
+	AFDT_R5G6B5_UINT,
+	AFDT_R32G32B32A32_FLOAT,
+	AFDT_R16G16B16A16_FLOAT,
+	AFDT_DEPTH,
+	AFDT_DEPTH_STENCIL,
+};
+//GLuint afCreateDynamicTexture(int w, int h, AFDTFormat format);
+
+class AFRenderTarget
+{
+	ivec2 texSize;
+	ID3D11RenderTargetView* renderTargetView = nullptr;
+	ID3D11ShaderResourceView* shaderResourceView = nullptr;
+	ID3D11UnorderedAccessView* unorderedAccessView = nullptr;
+	ID3D11DepthStencilView* depthStencilView = nullptr;
+public:
+	~AFRenderTarget() { Destroy(); }
+	void InitForDefaultRenderTarget();
+	void Init(ivec2 size, DXGI_FORMAT colorFormat);
+	void Destroy();
+	void BeginRenderToThis();
+	ID3D11ShaderResourceView* GetTexture() { return shaderResourceView; }
+	ID3D11UnorderedAccessView* GetUnorderedAccessView() { return unorderedAccessView; }
+};
