f39fff58b06781f6077b8d02dde8caacdf23dc67
yorung <yorung@gmail.com>
2016-02-23 01:48:04 +0900

    Hakodate's Little Planet

--- pack/assets/lua/start.lua
+++ pack/assets/lua/start.lua
@@ -15,6 +15,7 @@ AddMenu("WaterSurface", "dofile('lua/water_surface.lua')")
 AddMenu("Msg", "MessageBox('message', 'ok')")
 AddMenu("Jiji", "dofile('lua/jiji.lua')")
 AddMenu("Instancing", "dofile('lua/instancing.lua')")
+AddMenu("LittlePlanet", "LoadSkyBox('hakodate.jpg', 'projection_equirectangular_to_stereographic')")
 
 LoadSkyBox("hakodate.jpg", "sky_photosphere")
 
