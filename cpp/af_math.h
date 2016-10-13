3c64fc1870c48aa01fd61ea4e1e8b109d04c6f82
yorung <yorung@gmail.com>
2016-10-14 00:48:14 +0900

    Fix Vulkan NDC bugs.

--- cpp/af_math.h
+++ cpp/af_math.h
@@ -504,7 +504,7 @@ inline Mat perspectiveRH(affloat fov, affloat aspect, affloat n, affloat f)
 inline Mat ortho(affloat left, affloat right, affloat bottom, affloat top, affloat n, affloat f)
 {
 #if VK_TRUE
-	Mat proj = Mat(2 / (right - left), 0, 0, 0, 0, -2 / (top - bottom), 0, 0, 0, 0, 1 / (f - n), 0, (left + right) / (left - right), (bottom + top) / (bottom - top), n / (n - f), 1);
+	Mat proj = Mat(2 / (right - left), 0, 0, 0, 0, 2 / (bottom - top), 0, 0, 0, 0, 1 / (f - n), 0, (left + right) / (left - right), (bottom + top) / (top - bottom), n / (n - f), 1);
 #elif GL_TRUE
 	Mat proj = Mat(2 / (right - left), 0, 0, 0, 0, 2 / (top - bottom), 0, 0, 0, 0, -2 / (f - n), 0, (left + right) / (left - right), (bottom + top) / (bottom - top), (n + f) / (n - f), 1);
 #else
@@ -534,7 +534,11 @@ inline Mat makeViewportMatrix(const Vec2& screenSize)
 	Mat vp;
 	Vec2 sz = screenSize / 2;
 	vp._11 = sz.x;
+#if VK_TRUE
+	vp._22 = sz.y;
+#else
 	vp._22 = -sz.y;
+#endif
 	vp._41 = sz.x;
 	vp._42 = sz.y;
 	return vp;
