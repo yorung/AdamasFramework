11758303aecbd8770e7a8b5f7b9168250a842ab3
yorung <yorung@gmail.com>
2017-06-19 00:02:33 +0900

    [DX12] Support to create SRV from typeless formats.

--- cpp/helper_dx12.cpp
+++ cpp/helper_dx12.cpp
@@ -331,21 +331,22 @@ IVec2 afGetTextureSize(SRVID tex)
 static void AssignSRV(D3D12_CPU_DESCRIPTOR_HANDLE ptr , ComPtr<ID3D12Resource> res)
 {
 	D3D12_RESOURCE_DESC resDesc = res->GetDesc();
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 	if (resDesc.DepthOrArraySize == 6)
 	{
-		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 		srvDesc.Format = resDesc.Format;
 		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURECUBE;
 		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 		srvDesc.TextureCube.MipLevels = resDesc.MipLevels;
-		srvDesc.TextureCube.MostDetailedMip = 0;
-		srvDesc.TextureCube.ResourceMinLODClamp = 0;
-		deviceMan.GetDevice()->CreateShaderResourceView(res.Get(), &srvDesc, ptr);
 	}
 	else
 	{
-		deviceMan.GetDevice()->CreateShaderResourceView(res.Get(), nullptr, ptr);
+		srvDesc.Format = afTypelessToSRVFormat(resDesc.Format);
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		srvDesc.Texture2D.MipLevels = resDesc.MipLevels;
 	}
+	deviceMan.GetDevice()->CreateShaderResourceView(res.Get(), &srvDesc, ptr);
 }
 
 void afBindTexture(SRVID srv, int rootParameterIndex)
